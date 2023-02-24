517e9f283a019e5579514ea206507f21a4ca2796
Abdul <elizadeh9797@gmail.com>
2023-02-24 23:28:37 +0400

    All Tests passed...

--- src/Proje1/Proje1.java
+++ src/Proje1/Proje1.java
@@ -86,12 +86,82 @@ public class Proje1 extends BaseDriver {
 
         WebElement dash = driver.findElement(By.xpath("//h1[text()='Dashboard']"));
 
+
         Assert.assertTrue("login olunamdi", dash.getText().equals("Dashboard"));
+
+        WebElement logout = driver.findElement(By.xpath("//*[text()='Log out']"));
+        logout.click();
 
 
         MyFunction.Bekle(2);
 
 
+    }
+
+    @Test
+    public void Test3() {
+
+        driver.get("https://itera-qa.azurewebsites.net/");
+
+        WebElement login = driver.findElement(By.xpath("//*[text()='Login']"));
+        login.click();
+
+        WebElement username = driver.findElement(By.xpath("//*[@id='Username']"));
+        username.sendKeys(str);
+
+        WebElement password = driver.findElement(By.xpath("//*[@id='Password']"));
+        password.sendKeys(password1);
+
+        WebElement login2 = driver.findElement(By.xpath("//*[@value='Login']"));
+        login2.click();
+
+
+        WebElement createNew = driver.findElement(By.xpath("//*[text()='Create New']"));
+        createNew.click();
+
+        WebElement customerName = driver.findElement(By.xpath("//*[@id='Name']"));
+        customerName.sendKeys("Doga");
+
+        String company = RandomString.make(10);
+        WebElement companyName = driver.findElement(By.xpath("//*[@id='Company']"));
+        companyName.sendKeys(company);
+
+        WebElement address = driver.findElement(By.xpath("//*[@id='Address']"));
+        address.sendKeys("sdsasasasa");
+
+        WebElement city = driver.findElement(By.xpath("//*[@id='City']"));
+        city.sendKeys("denene");
+
+        WebElement phone = driver.findElement(By.xpath("//*[@id='Phone']"));
+        phone.sendKeys("1111111111");
+
+        String mail = RandomString.make(8)+"@adads.com";
+
+        WebElement email = driver.findElement(By.xpath("//*[@id='Email']"));
+        email.sendKeys(mail);
+
+        WebElement create = driver.findElement(By.xpath("//*[@value='Create']"));
+        create.click();
+
+
+        WebElement searching = driver.findElement(By.xpath("//*[@id='searching']"));
+        searching.sendKeys(mail);
+
+        MyFunction.Bekle(2);
+
+        WebElement searchingb = driver.findElement(By.xpath("//*[@value='Search']"));
+        searchingb.click();
+
+        MyFunction.Bekle(2);
+
+        WebElement company1 = driver.findElement(By.xpath("//*[@class='table-primary']/following-sibling::tr/td[2]"));
+
+
+
+        Assert.assertTrue("Company bulunamdi", company1.getText().equals(company));
+
+
+        MyFunction.Bekle(1);
         BekleKapat();
     }
 
