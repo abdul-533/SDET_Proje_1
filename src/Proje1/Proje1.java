eb340fede4507cf86ab2e03b8050ce77a15865dc
Abdul <elizadeh9797@gmail.com>
2023-02-24 20:29:23 +0400

    Our project 1 is created...

---
+++ src/Proje1/Proje1.java
@@ -0,0 +1,14 @@
+package Proje1;
+
+
+import Utility.BaseDriver;
+public class Proje1  extends BaseDriver {
+
+    public static void main(String[] args) {
+       driver.get("https://itera-qa.azurewebsites.net/");
+
+
+
+    }
+
+}
